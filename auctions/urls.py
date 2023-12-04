f473989a844ec5faae9d9274affee308e7ace166
MichaelHorak <118129078+MichaelHorak@users.noreply.github.com>
2023-12-04 17:38:57 +0100

    base

---
+++ auctions/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = "auctions"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("create listing", views.create_listing, name="create listing"),
+    path("categories", views.categories, name="categories"),
+    path("watchlist", views.watchlist, name="watchlist")
+]
